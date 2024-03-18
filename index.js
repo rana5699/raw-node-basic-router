4a5a2d2cc99cd7cd56d352fd5442a94a134be409
rana5699 <ranaot56@gmail.com>
2024-03-18 22:16:04 +0600

    try another way

--- index.js
+++ index.js
@@ -1,37 +1,66 @@
-const http = require("http"); //require http and npm init
-const fs = require("fs"); //require fs(fileSystem)
+// const http = require("http");
+// const fs = require("fs");
 
-const PORT = 3000;
-const hosaName = "127.0.0.1";
+// const port = 3007;
+// const hostName = "127.0.0.1";
 
-// ! create server
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     fs.readFile("./pages/home.html", (err, data) => {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     });
+//   } else if (req.url === "/about") {
+//     fs.readFile("./pages/about.html", (err, data) => {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     });
+//   } else if (req.url === "/contact") {
+//     fs.readFile("./pages/contacat.html", (err, data) => {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     });
+//   } else {
+//     fs.readFile("./pages/error.html", (err, data) => {
+//       res.writeHead(404, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+// });
+
+// server.listen(port, hostName, () => {
+//   console.log(`Server run at this http://${hostName}:${port}`);
+// });
+
+const http = require("http");
+const fs = require("fs");
+
+const port = 3009;
+const hostName = "127.0.0.1";
+
 const server = http.createServer((req, res) => {
+  const handleReadFile = (filePath, statusCode) => {
+    fs.readFile(filePath, (err, data) => {
+      res.writeHead(statusCode, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  };
+
   if (req.url === "/") {
-    fs.readFile("./pages/home.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    handleReadFile("./pages/home.html", 200);
   } else if (req.url === "/about") {
-    fs.readFile("./pages/about.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    handleReadFile("./pages/about.html", 200);
   } else if (req.url === "/contact") {
-    fs.readFile("./pages/contacat.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    handleReadFile("./pages/contacat.html", 200);
   } else {
-    fs.readFile("./pages/error.html", (err, data) => {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    handleReadFile("./pages/error.html", 404);
   }
 });
-server.listen(PORT, hosaName, () => {
-  console.log(`Server run at http://${hosaName}:${PORT}`);
+server.listen(port, hostName, () => {
+  console.log(`run at this http://${hostName}:${port}`);
 });
